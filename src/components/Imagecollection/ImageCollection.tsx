396eff776ba69780a9789f874e95c5f07157739f
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-03 05:53:06 +0000

    named collection in collections page

--- src/components/Imagecollection/ImageCollection.tsx
+++ src/components/Imagecollection/ImageCollection.tsx
@@ -15,54 +15,59 @@ export default function ImageCollection({
   return collections.map((collection) => {
     return (
       <Fragment key={collection.id}>
-        <div className="w-full h-72 lg:h-80 xl:h-96 rounded lg:rounded-lg flex flex-col gap-2 lg:gap-4 xl:gap-6 overflow-hidden">
-          <Link
-            href={`/collection/${collection.id}`}
-            className="relative flex-grow h-full w-full grid grid-rows-6 gap-1 xl:gap-2 overflow-hidden transition-all before:hidden hover:before:block before:content-normal before:h-full before:w-full before:absolute before:inset-0 before:bg-[rgba(0,0,0,.15)]"
-          >
-            <div className="row-span-4 rounded lg:rounded-lg">
-              <Image
-                src={collection.images.media[0].src.large}
-                alt={collection.images.media[0].alt}
-                height={collection.images.media[0].height}
-                width={collection.images.media[0].width}
-                quality={100}
-                className="h-full w-full object-cover rounded-tl rounded-tr lg:rounded-tl-lg lg:rounded-tr-lg"
-              />
-            </div>
-            <div className="row-span-2 flex gap-1 xl:gap-2">
-              <div className="h-full w-full">
+        <div>
+          <div className="w-full h-72 lg:h-80 xl:h-96 rounded lg:rounded-lg flex flex-col gap-2 lg:gap-4 xl:gap-6 overflow-hidden">
+            <Link
+              href={`/collection/${collection.id}`}
+              className="relative flex-grow h-full w-full grid grid-rows-6 gap-1 xl:gap-2 overflow-hidden transition-all before:hidden hover:before:block before:content-normal before:h-full before:w-full before:absolute before:inset-0 before:bg-[rgba(0,0,0,.15)]"
+            >
+              <div className="row-span-4 rounded lg:rounded-lg">
                 <Image
-                  src={collection.images.media[1].src.small}
-                  alt={collection.images.media[1].alt}
-                  height={collection.images.media[1].height}
-                  width={collection.images.media[1].width}
-                  quality={90}
-                  className="h-full w-full object-cover rounded-bl lg:rounded-bl-lg overflow-clip"
+                  src={collection.images.media[0].src.large}
+                  alt={collection.images.media[0].alt}
+                  height={collection.images.media[0].height}
+                  width={collection.images.media[0].width}
+                  quality={100}
+                  className="h-full w-full object-cover rounded-tl rounded-tr lg:rounded-tl-lg lg:rounded-tr-lg"
                 />
               </div>
-              <div className="h-full w-full">
-                <Image
-                  src={collection.images.media[2].src.small}
-                  alt={collection.images.media[2].alt}
-                  height={collection.images.media[2].height}
-                  width={collection.images.media[2].width}
-                  quality={90}
-                  className="h-full w-full object-cover overflow-clip"
-                />
+              <div className="row-span-2 flex gap-1 xl:gap-2">
+                <div className="h-full w-full">
+                  <Image
+                    src={collection.images.media[1].src.small}
+                    alt={collection.images.media[1].alt}
+                    height={collection.images.media[1].height}
+                    width={collection.images.media[1].width}
+                    quality={90}
+                    className="h-full w-full object-cover rounded-bl lg:rounded-bl-lg overflow-clip"
+                  />
+                </div>
+                <div className="h-full w-full">
+                  <Image
+                    src={collection.images.media[2].src.small}
+                    alt={collection.images.media[2].alt}
+                    height={collection.images.media[2].height}
+                    width={collection.images.media[2].width}
+                    quality={90}
+                    className="h-full w-full object-cover overflow-clip"
+                  />
+                </div>
+                <div className="h-full w-full">
+                  <Image
+                    src={collection.images.media[3].src.small}
+                    alt={collection.images.media[3].alt}
+                    height={collection.images.media[3].height}
+                    width={collection.images.media[3].width}
+                    quality={90}
+                    className="h-full w-full object-cover rounded-br lg:rounded-br-lg overflow-clip"
+                  />
+                </div>
               </div>
-              <div className="h-full w-full">
-                <Image
-                  src={collection.images.media[3].src.small}
-                  alt={collection.images.media[3].alt}
-                  height={collection.images.media[3].height}
-                  width={collection.images.media[3].width}
-                  quality={90}
-                  className="h-full w-full object-cover rounded-br lg:rounded-br-lg overflow-clip"
-                />
-              </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
+          <h2 className="mx-1 lg:mx-2 py-2 lg:py-4 text-lg xl:text-xl text-gray-400">
+            {collection.name}
+          </h2>
         </div>
       </Fragment>
     );
