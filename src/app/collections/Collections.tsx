7ea27a48ea32928c3de1c7543259b113265ccf94
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-03 05:52:47 +0000

    code moved to page

--- src/app/collections/Collections.tsx
+++ src/app/collections/Collections.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect, useState } from "react";
-import Header from "@/components/Header/Header";
 import ImageCollection from "@/components/Imagecollection/ImageCollection";
 import { useFetch } from "@/hooks/useFetch";
 import { type PhotoCollectionType } from "@/shared/types/collection";
@@ -23,9 +22,9 @@ export default function Collections() {
     };
     fetchCollection();
 
-    return () =>{
+    return () => {
       controller.abort();
-    }
+    };
   }, [page, fetchResource]);
 
   // update collections
@@ -37,24 +36,9 @@ export default function Collections() {
 
   return (
     <>
-      <main className="w-full">
-        <Header />
-        <section className="w-full max-w-screen-2xl mx-auto px-6 py-6 lg:py-12 xl:py-16">
-          <div className="w-full flex justify-center items-center">
-            <h1 className="text-xl lg:text-2xl xl:text-4xl font-semibold xl:font-bold text-gray-300">
-              Discover Collections
-            </h1>
-          </div>
-          <section
-            className="w-full py-8 lg:py-12 xl:py-16"
-            aria-label="collection-grid"
-          >
-            <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 lg:gap-8 lg:gap-y-12 xl:gap-12 xl:gap-y-16">
-              <ImageCollection collections={collections} />
-            </section>
-          </section>
-        </section>
-      </main>
+      <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 lg:gap-8 lg:gap-y-12 xl:gap-12 xl:gap-y-16">
+        <ImageCollection collections={collections} />
+      </section>
     </>
   );
 }
--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Header from "@/components/Header/Header";
 import Collections from "./Collections";
 
 export const metadata: Metadata = {
@@ -20,7 +21,22 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <Collections />
+      <main className="w-full">
+        <Header />
+        <section className="w-full max-w-screen-2xl mx-auto px-6 py-6 lg:py-12 xl:py-16">
+          <div className="w-full flex justify-center items-center">
+            <h1 className="text-xl lg:text-2xl xl:text-4xl font-semibold xl:font-bold text-gray-300">
+              Discover Collections
+            </h1>
+          </div>
+          <section
+            className="w-full py-8 lg:py-12 xl:py-16"
+            aria-label="collection-grid"
+          >
+            <Collections />
+          </section>
+        </section>
+      </main>
     </>
   );
 }
