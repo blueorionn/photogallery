cebc80dfab9cf6761a5a69c07fd90ed26a0dd113
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-03 12:57:53 +0000

    set initial theme

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,12 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useDarkMode() {
   const [theme, setTheme] = useState<"light" | "dark">("light");
 
+  // set initial theme
+  useEffect(() => {
+    const setInitialTheme = () => {
+      // preferred theme
+      if (typeof window !== undefined) {
+        const isDarkOs = window.matchMedia(
+          "(prefers-color-scheme: dark)"
+        ).matches;
+        if (isDarkOs) {
+          setTheme("dark");
+        }
+      }
+    };
+    setInitialTheme();
+  }, []);
+
+  // toggle theme button
   const toggleTheme = () => {
     if (theme === "dark") {
       setTheme("light");
-    } else if(theme == 'light') {
+    } else if (theme == "light") {
       setTheme("dark");
     }
   };
