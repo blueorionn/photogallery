c7d4e4e589c006233729360540d5b08991b652c7
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-03 13:21:29 +0000

    add page loading to collections

--- src/app/collections/Collections.tsx
+++ src/app/collections/Collections.tsx
@@ -3,17 +3,32 @@ import { useEffect, useState } from "react";
 import ImageCollection from "@/components/Imagecollection/ImageCollection";
 import { useFetch } from "@/hooks/useFetch";
 import { type PhotoCollectionType } from "@/shared/types/collection";
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 
 export default function Collections() {
+  // photo collections
   const [collections, setCollections] =
     useState<{ name: string; id: string; images: PhotoCollectionType }[]>();
-  const [page] = useState(1);
+
+  // current api page
+  const [page, setPage] = useState(1);
+
+  // freeze fetching if reached page limit
+  const [frozen] = useState(false);
+
+  // intersection observer
+  const { ref, isIntersecting } = useIntersectionObserver<HTMLDivElement>();
+
+  // fetched data
   const { data, fetchResource } = useFetch<{
     result: { name: string; id: string; images: PhotoCollectionType }[];
   }>();
 
   // fetch collection
   useEffect(() => {
+    // don't fetch if page limit reached.
+    if (frozen) return;
+
     const controller = new AbortController();
     const signal = controller.signal;
 
@@ -25,7 +40,7 @@ export default function Collections() {
     return () => {
       controller.abort();
     };
-  }, [page, fetchResource]);
+  }, [page, fetchResource, frozen]);
 
   // update collections
   useEffect(() => {
@@ -34,11 +49,19 @@ export default function Collections() {
     }
   }, [data]);
 
+  // increment page if intersection
+  useEffect(() => {
+    if (isIntersecting) {
+      setPage((page) => page++);
+    }
+  }, [isIntersecting]);
+
   return (
     <>
       <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 lg:gap-8 lg:gap-y-12 xl:gap-12 xl:gap-y-16">
         <ImageCollection collections={collections} />
       </section>
+      <div aria-label="loader" className="my-4 xl:my-6" ref={ref}></div>
     </>
   );
 }
---
+++ src/hooks/useIntersectionObserver.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState, useRef, useMemo } from "react";
+
+type IntersectionObserverOptions = {
+  root?: Element | null;
+  rootMargin?: string;
+  threshold?: number | number[];
+};
+
+type UseIntersectionObserverReturn<T extends HTMLElement> = {
+  ref: React.RefObject<T | null>;
+  isIntersecting: boolean;
+};
+
+export function useIntersectionObserver<T extends HTMLElement>(
+  options: IntersectionObserverOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  }
+): UseIntersectionObserverReturn<T> {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef<T>(null);
+
+  // memorized options
+  const memoizedOptions = useMemo(() => options, [options]);
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    }, memoizedOptions);
+
+    observer.observe(element);
+
+    return () => {
+      if (element) {
+        observer.unobserve(element);
+      }
+    };
+  }, [ref, memoizedOptions]);
+
+  return { ref, isIntersecting };
+}
