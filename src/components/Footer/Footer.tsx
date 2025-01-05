37caf37c6c19bbf1ce70c96052befbcd7fda682e
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-05 06:22:08 +0000

    modified footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
             >
               <span className="sr-only">Github repo url</span>
               <svg
-                className="fill-gray-500 h-4 w-4 md:h-6 md:w-6 lg:h-8 lg:w-8 hover:fill-gray-700 hover:dark:fill-gray-300 transition-all"
+                className="fill-gray-500 h-6 w-6 lg:h-8 lg:w-8 hover:fill-gray-700 hover:dark:fill-gray-300 transition-all"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 100 100"
               >
@@ -31,13 +31,11 @@ export default function Footer() {
             >
               <span className="sr-only">My Twitter profile</span>
               <svg
-                className="fill-gray-500 h-3 w-3 md:h-5 md:w-5 lg:h-7 lg:w-7 hover:fill-gray-700 hover:dark:fill-gray-300 transition-all"
+                className="fill-gray-500 h-4 w-4 md:h-5 md:w-5 lg:h-7 lg:w-7 hover:fill-gray-700 hover:dark:fill-gray-300 transition-all"
                 viewBox="0 0 1200 1227"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path
-                  d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
-                />
+                <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />
               </svg>
             </Link>
           </div>
