3bfd308682fc7563277bc796d36160a1f7e87bd6
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-13 17:11:44 +0000

    wrapping LoginPage component on Suspense

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import { login } from "./actions";
 import LoginPage from "@/www/Login";
 
 export const metadata: Metadata = {
-  title: "Login | Photogallery"
+  title: "Login | Photogallery",
 };
 
 export default function Page() {
@@ -11,7 +12,9 @@ export default function Page() {
     <>
       <main className="h-full w-full">
         <section className="h-full w-full grid place-items-center">
-          <LoginPage login={login} />
+          <Suspense>
+            <LoginPage login={login} />
+          </Suspense>
         </section>
       </main>
     </>
