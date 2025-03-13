98da4e736c0f2cf6141d922dfc9fc1192eb75362
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-13 13:36:29 +0000

    code format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,15 +11,17 @@ export const metadata: Metadata = {
     description:
       "This website is a casual portfolio project created to explore and display the beauty of photography. All the photos and videos showcased here are fetched using the Pexels API, a platform that provides high-quality and free-to-use images and videos from talented photographers around the world.",
     url: "https://photogallery.functionbasket.com/",
-    images: "https://photogallery.functionbasket.com/img/photogallery-site-image.png"
+    images:
+      "https://photogallery.functionbasket.com/img/photogallery-site-image.png",
   },
   twitter: {
     card: "summary_large_image",
     title: "Photogallery | Hobby Project",
     description:
-    "This website is a casual portfolio project created to explore and display the beauty of photography. All the photos and videos showcased here are fetched using the Pexels API, a platform that provides high-quality and free-to-use images and videos from talented photographers around the world.",
+      "This website is a casual portfolio project created to explore and display the beauty of photography. All the photos and videos showcased here are fetched using the Pexels API, a platform that provides high-quality and free-to-use images and videos from talented photographers around the world.",
     site: "@SS_Swadhinn",
-    images: "https://photogallery.functionbasket.com/img/photogallery-site-image.png"
+    images:
+      "https://photogallery.functionbasket.com/img/photogallery-site-image.png",
   },
 };
 
--- src/utils/func.ts
+++ src/utils/func.ts
@@ -1,17 +1,16 @@
 // Generate random char
 export function generateRandomString(length = 7) {
-    const chars =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let result = "";
-    for (let i = 0; i < length; i++) {
-      result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return result;
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
-  
-  // set localStorage value
-  export function setLocalStorageFunc(key: string, value: string) {
-    if (typeof window === undefined) return;
-    localStorage.setItem(key, value);
-  }
-  
+  return result;
+}
+
+// set localStorage value
+export function setLocalStorageFunc(key: string, value: string) {
+  if (typeof window === undefined) return;
+  localStorage.setItem(key, value);
+}
