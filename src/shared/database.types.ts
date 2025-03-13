e38e0950e0ec465d19d0f74afb46bba7e0b55efb
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-13 18:33:17 +0000

    created api

---
+++ src/shared/database.types.ts
@@ -0,0 +1,29 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
+
+export interface Database {
+  public: {
+    Tables: {
+      movies: {
+        Row: {
+          // the data expected from .select()
+          id: number;
+          uuid: string;
+          photoname: string;
+          tag: string | null;
+        };
+        Update: {
+          // the data to be passed to .update()
+          id: number;
+          uuid?: string; // `not null` columns are optional on .update()
+          tag: string;
+        };
+      };
+    };
+  };
+}
