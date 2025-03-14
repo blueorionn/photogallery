a7e78e7ca1795e6c683e25a4dc23f0c9ee4506bc
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-14 11:09:32 +0000

    renamed env variables

--- src/shared/supabase.ts
+++ src/shared/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./database.types";
 
 export const supabaseDbClient = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PRIVATE_SUPABASE_URL!,
   process.env.NEXT_PRIVATE_SUPABASE_SERVICE_ROLE!
 );
