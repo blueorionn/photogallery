469e87fa473af66625edbae96f14b06815cf9321
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-04-15 17:42:46 +0000

    feat: configured prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 const title = 'PhotoGallery | Explore. Admire. Repeat.'
-const description = 'Explore a curated collection of stunning photos from around the world. PhotoGallery brings visual inspiration to your screen—one snapshot at a time.'
+const description =
+  'Explore a curated collection of stunning photos from around the world. PhotoGallery brings visual inspiration to your screen—one snapshot at a time.'
 
 export const metadata: Metadata = {
   title: title,
@@ -21,33 +22,29 @@ export const metadata: Metadata = {
   openGraph: {
     title: title,
     description: description,
-    images: [
-      'https://photogallery.functionbasket.com/logo.png',
-    ],
+    images: ['https://photogallery.functionbasket.com/logo.png'],
     url: 'https://photogallery.functionbasket.com',
   },
   twitter: {
     card: 'summary',
     title: title,
     description: description,
-    images: [
-      'https://photogallery.functionbasket.com/logo.png',
-    ]
-  }
-};
+    images: ['https://photogallery.functionbasket.com/logo.png'],
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
-  );
+  )
 }
