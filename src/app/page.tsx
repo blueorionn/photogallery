1315bc0962923db66bdb8b945996ffb242136789
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-04-16 16:57:18 +0000

    feat: added auth on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import Image from 'next/image'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { parse } from 'cookie'
+import { isSessionValid } from '@/auth/auth'
 
-export default function Home() {
+export default async function Home() {
+  // Check if user is logged in
+  const headerList = await headers()
+  const cookieHeader = await headerList.get('cookie')
+  const cookies = cookieHeader ? parse(cookieHeader) : {}
+  const sessionId = cookies['sessionid']
+
+  // Check if session ID is present
+  if (!sessionId || !(await isSessionValid(sessionId))) {
+    return redirect('/login')
+  }
+
   return (
     <div className='grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
       <main className='row-start-2 flex flex-col items-center gap-[32px] sm:items-start'>
