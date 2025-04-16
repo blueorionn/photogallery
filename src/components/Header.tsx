6ca05c795a82fa1773bb9acf267902947eca882f
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-04-16 17:36:10 +0000

    feat: created header

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import { Space_Grotesk } from 'next/font/google'
+
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
+
+export default function Header() {
+  return (
+    <>
+      <header className='w-full bg-gray-950 p-8'>
+        <div className='mx-auto flex max-w-7xl items-center justify-start gap-4'>
+          <Image
+            src={'/android-chrome-192x192.png'}
+            alt='Site Logo'
+            height={192}
+            width={192}
+            className='aspect-auto h-8 w-8'
+          />
+          <h1
+            className={`${spaceGrotesk.className} text-lg font-bold text-gray-200 xl:text-xl`}
+          >
+            Photogallery
+          </h1>
+        </div>
+      </header>
+    </>
+  )
+}
