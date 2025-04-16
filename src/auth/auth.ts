feb8d6890015890a3b519a4f04391327b084f21a
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-04-16 17:20:03 +0000

    feat: completed auth middleware

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,9 +2,9 @@ import { query } from '@/lib/db'
 
 export async function isSessionValid(sessionid: string) {
   const session = await query(
-    'SELECT * FROM sessions WHERE sessionid = $1 AND expiry_date > CURRENT_DATE',
+    'SELECT * FROM sessions WHERE id = $1 AND expiry_date > CURRENT_DATE',
     [sessionid]
   )
-  console.log(session)
+  if ((session.rowCount || 0) > 0) return true
   return false
 }
